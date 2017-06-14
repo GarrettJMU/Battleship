74b5133bcc64691387caf00180f71311fcc31812
learn <learn@learn02.local>
2017-06-14 11:26:18 -0700

    Initial commit with working board

---
+++ battleship.js
@@ -0,0 +1,36 @@
+var board = []
+var counter = 0
+var player1 = 1
+var player2 = -1
+var torpedoes = 1
+var ship1 = 5
+var ship2 = 4
+var ship3 = 3
+var ship4 = 3
+var ship5 = 2
+
+
+$(document).ready(function() {
+  var table = $('<table></table>').addClass("board"); //selects the table element in our html
+  for(i=0; i<10; i++){ //inside the table, generate the first
+    var row = $('<tr>').addClass("row" + i);
+    table.append(row); //Updates the rows
+    for(j=0; j<10; j++){
+      var col = $('<td></td>').attr('id',("col"+ counter++));
+      row.append(col); //keeps all of the columns in rows
+    }
+}
+$('#boardDiv').append(table);
+
+  $("td").click(function(){
+    $(this).addClass("torpedoShot")
+    $(".torpedoesUsed").text(("Torpedoes Used: " + torpedoes++))
+  }) //Is the counter for torpedoes Used
+
+  $(".reset").click(function(){
+    $("td").removeClass("torpedoShot","torpedohit")
+    $(".torpedoesUsed").text(("Torpedoes Used: "))
+    torpedoes = 1;
+  })
+
+});
